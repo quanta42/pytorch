bae4f0a1d7cc00a9dc7aaa015f6495b987bd726b
Lourenço Matos <lourenco.matos@tecnico.ulisboa.pt>
2024-06-04 03:46:07 +0100

    ran linter (again)

--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -4011,7 +4011,6 @@ def pad_adaptive_loader(x, pad_val=0.0):
 
 
 def compute_indices_adaptive_pooling(start_index, end_index, h_in, w_in, h_out, w_out):
-
     h_start_index = functools.partial(start_index, out_dim=h_out, inp_dim=h_in)
     h_end_index = functools.partial(end_index, out_dim=h_out, inp_dim=h_in)
 
@@ -4021,15 +4020,20 @@ def compute_indices_adaptive_pooling(start_index, end_index, h_in, w_in, h_out, 
     return h_start_index, h_end_index, w_start_index, w_end_index
 
 
-def _adaptive_pooling_fn(start_index, end_index, kernel_maxes, in_sizes, out_sizes, pooling_fn):
-
+def _adaptive_pooling_fn(
+    start_index, end_index, kernel_maxes, in_sizes, out_sizes, pooling_fn
+):
     h_in, w_in = in_sizes
     h_out, w_out = out_sizes
 
-    (h_start_index_fn,
-     h_end_index_fn,
-     w_start_index_fn,
-     w_end_index_fn) = compute_indices_adaptive_pooling(start_index, end_index, h_in, w_in, h_out, w_out)
+    (
+        h_start_index_fn,
+        h_end_index_fn,
+        w_start_index_fn,
+        w_end_index_fn,
+    ) = compute_indices_adaptive_pooling(
+        start_index, end_index, h_in, w_in, h_out, w_out
+    )
 
     def fn(idx, loader):
         *prefix, bh, bw = idx
@@ -4057,15 +4061,20 @@ def _adaptive_pooling_fn(start_index, end_index, kernel_maxes, in_sizes, out_siz
     return fn
 
 
-def _adaptive_pooling_fn_with_idx(start_index, end_index, kernel_maxes, in_sizes, out_sizes, pooling_fn):
-
+def _adaptive_pooling_fn_with_idx(
+    start_index, end_index, kernel_maxes, in_sizes, out_sizes, pooling_fn
+):
     h_in, w_in = in_sizes
     h_out, w_out = out_sizes
 
-    (h_start_index_fn,
-     h_end_index_fn,
-     w_start_index_fn,
-     w_end_index_fn) = compute_indices_adaptive_pooling(start_index, end_index, h_in, w_in, h_out, w_out)
+    (
+        h_start_index_fn,
+        h_end_index_fn,
+        w_start_index_fn,
+        w_end_index_fn,
+    ) = compute_indices_adaptive_pooling(
+        start_index, end_index, h_in, w_in, h_out, w_out
+    )
 
     def fn(idx, loader):
         *prefix, bh, bw = idx
@@ -4254,16 +4263,10 @@ def adaptive_max_pool2d(x, output_size):
     )
 
     def inner_fn_max_val(idx):
-        return inner_func_max_val(
-            idx,
-            pad_adaptive_loader(x, float("-inf"))
-        )
+        return inner_func_max_val(idx, pad_adaptive_loader(x, float("-inf")))
 
     def inner_fn_max_idx(idx):
-        return inner_func_max_idx(
-            idx,
-            pad_adaptive_loader(x, float("-inf"))
-        )
+        return inner_func_max_idx(idx, pad_adaptive_loader(x, float("-inf")))
 
     rv = Pointwise.create(
         device=x.get_device(),
@@ -4796,7 +4799,9 @@ def avg_pool3d_backward(
     *batch, depth, height, width = x.get_size()
 
     d_out, ceil_mode_d = pooling_size(depth, 0, kernel_size, stride, padding, ceil_mode)
-    h_out, ceil_mode_h = pooling_size(height, 1, kernel_size, stride, padding, ceil_mode)
+    h_out, ceil_mode_h = pooling_size(
+        height, 1, kernel_size, stride, padding, ceil_mode
+    )
     w_out, ceil_mode_w = pooling_size(width, 2, kernel_size, stride, padding, ceil_mode)
 
     grad_loader = grad_output.make_loader()
@@ -4806,11 +4811,13 @@ def avg_pool3d_backward(
     new_size = list(x.get_size())
     dtype = x.get_dtype()
 
-    d_window_size, h_window_size, w_window_size = [
-        max(max(d // stride[i] - max(0, (d - kernel_size[i]) // stride[i]), 1)
-            for d in range(kernel_size[i] * 2))
+    d_window_size, h_window_size, w_window_size = (
+        max(
+            max(d // stride[i] - max(0, (d - kernel_size[i]) // stride[i]), 1)
+            for d in range(kernel_size[i] * 2)
+        )
         for i in range(3)
-    ]
+    )
 
     window_size = d_window_size * h_window_size * w_window_size
     if window_size > 125:
@@ -4829,45 +4836,63 @@ def avg_pool3d_backward(
     def compute_pool_size_without_padding(pd, ph, pw):
         stride_d, stride_h, stride_w = (ops.constant(s, torch.int32) for s in stride)
         pad_d, pad_h, pad_w = (ops.constant(p, torch.int32) for p in padding)
-        kernel_d, kernel_h, kernel_w = (ops.constant(k, torch.int32) for k in kernel_size)
+        kernel_d, kernel_h, kernel_w = (
+            ops.constant(k, torch.int32) for k in kernel_size
+        )
 
         dstart, hstart, wstart = (
             ops.sub(ops.mul(p, s), pad)
-            for p, s, pad in zip([pd, ph, pw], [stride_d, stride_h, stride_w], [pad_d, pad_h, pad_w])
+            for p, s, pad in zip(
+                [pd, ph, pw], [stride_d, stride_h, stride_w], [pad_d, pad_h, pad_w]
+            )
         )
         dend, hend, wend = (
             ops.minimum(
-                ops.add(start, k),
-                ops.add(ops.index_expr(dim, torch.int32), pad)
+                ops.add(start, k), ops.add(ops.index_expr(dim, torch.int32), pad)
             )
-            for start, k, dim, pad in zip([dstart, hstart, wstart], [kernel_d, kernel_h, kernel_w], [depth, height, width], [pad_d, pad_h, pad_w])
+            for start, k, dim, pad in zip(
+                [dstart, hstart, wstart],
+                [kernel_d, kernel_h, kernel_w],
+                [depth, height, width],
+                [pad_d, pad_h, pad_w],
+            )
         )
-        dstart, hstart, wstart = (ops.maximum(start, ops.constant(0, torch.int32)) for start in [dstart, hstart, wstart])
+        dstart, hstart, wstart = (
+            ops.maximum(start, ops.constant(0, torch.int32))
+            for start in [dstart, hstart, wstart]
+        )
         dend, hend, wend = (
             ops.minimum(end, ops.index_expr(dim, torch.int32))
             for end, dim in zip([dend, hend, wend], [depth, height, width])
         )
-        divide_factor = ops.mul(ops.mul(ops.sub(dend, dstart), ops.sub(hend, hstart)), ops.sub(wend, wstart))
+        divide_factor = ops.mul(
+            ops.mul(ops.sub(dend, dstart), ops.sub(hend, hstart)), ops.sub(wend, wstart)
+        )
         return divide_factor
 
     def fn(idx):
         *prefix, d, h, w = idx
         d, h, w = (v + pad for v, pad in zip([d, h, w], padding))
-       
+
         pdstart, phstart, pwstart = (
             ops.index_expr(FloorDiv(v - k + s, s), torch.int32)
             for v, k, s in zip([d, h, w], kernel_size, stride)
         )
-        
+
         pdend, phend, pwend = (
             ops.index_expr(FloorDiv(v, s) + 1, torch.int32)
             for v, s in zip([d, h, w], stride)
         )
 
-        pdstart, phstart, pwstart = (ops.maximum(pstart, ops.constant(0, torch.int32)) for pstart in [pdstart, phstart, pwstart])
+        pdstart, phstart, pwstart = (
+            ops.maximum(pstart, ops.constant(0, torch.int32))
+            for pstart in [pdstart, phstart, pwstart]
+        )
         pdend, phend, pwend = (
             ops.minimum(pend, ops.index_expr(pooled_dim, torch.int32))
-            for pend, pooled_dim in zip([pdend, phend, pwend], [pooled_depth, pooled_height, pooled_width])
+            for pend, pooled_dim in zip(
+                [pdend, phend, pwend], [pooled_depth, pooled_height, pooled_width]
+            )
         )
 
         gradient = None
@@ -4877,7 +4902,9 @@ def avg_pool3d_backward(
                 for pw_ in range(w_window_size):
                     pd, ph, pw = (
                         ops.add(pstart, ops.constant(p_, torch.int32))
-                        for pstart, p_ in zip([pdstart, phstart, pwstart], [pd_, ph_, pw_])
+                        for pstart, p_ in zip(
+                            [pdstart, phstart, pwstart], [pd_, ph_, pw_]
+                        )
                     )
 
                     if divisor_override is not None:
@@ -4892,17 +4919,23 @@ def avg_pool3d_backward(
                             [
                                 *prefix,
                                 ops.indirect_indexing(
-                                    ops.minimum(pd, ops.sub(pdend, ops.constant(1, torch.int32))),
+                                    ops.minimum(
+                                        pd, ops.sub(pdend, ops.constant(1, torch.int32))
+                                    ),
                                     pooled_depth,
                                     check=False,
                                 ),
                                 ops.indirect_indexing(
-                                    ops.minimum(ph, ops.sub(phend, ops.constant(1, torch.int32))),
+                                    ops.minimum(
+                                        ph, ops.sub(phend, ops.constant(1, torch.int32))
+                                    ),
                                     pooled_height,
                                     check=False,
                                 ),
                                 ops.indirect_indexing(
-                                    ops.minimum(pw, ops.sub(pwend, ops.constant(1, torch.int32))),
+                                    ops.minimum(
+                                        pw, ops.sub(pwend, ops.constant(1, torch.int32))
+                                    ),
                                     pooled_width,
                                     check=False,
                                 ),
@@ -4911,9 +4944,14 @@ def avg_pool3d_backward(
                         scale,
                     )
 
-                    mask = ops.and_(ops.and_(ops.lt(pd, pdend), ops.lt(ph, phend)), ops.lt(pw, pwend))
+                    mask = ops.and_(
+                        ops.and_(ops.lt(pd, pdend), ops.lt(ph, phend)),
+                        ops.lt(pw, pwend),
+                    )
                     if gradient is None:
-                        gradient = ops.where(mask, part, ops.constant(0.0, torch.float32))
+                        gradient = ops.where(
+                            mask, part, ops.constant(0.0, torch.float32)
+                        )
                     else:
                         gradient = ops.where(mask, ops.add(gradient, part), gradient)
         assert gradient is not None
