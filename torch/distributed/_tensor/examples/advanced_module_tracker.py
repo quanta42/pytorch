6981dca39d3d7f28a31105c3eec6684618c3a2fb
Anshul Sinha <50644008+sinhaanshul@users.noreply.github.com>
2024-06-05 12:05:08 -0700

    [dtensor][experiment] experimenting with displaying model parameters and printing sharding info
    ghstack-source-id: 3417f49bc7ae21c38bdc9536d97aa1688f3f1b35
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/127987

--- torch/distributed/_tensor/examples/advanced_module_tracker.py
+++ torch/distributed/_tensor/examples/advanced_module_tracker.py
@@ -14,6 +14,7 @@ class AdvancedModuleTracker(ModuleTracker):
     def __init__(self):
         super().__init__()
         self.module_parameters_dict = {}
+        self.sharding_dict = {}
 
     def _fw_pre_hook(self, mod, input):
         name = super()._get_mod_name(mod)
@@ -29,11 +30,22 @@ class AdvancedModuleTracker(ModuleTracker):
                 self.module_parameters_dict[name] = {}
 
             self.module_parameters_dict[name][param_name] = param.data
+            if type(param.data) == torch.distributed._tensor.api.DTensor:
+                key_name = name + "." + param_name
+                self.sharding_dict[key_name] = param.data.placements
 
     def __enter__(self):
         self.module_parameters_dict.clear()
+        self.sharding_dict.clear()
         self._fw_pre_handle = register_module_forward_pre_hook(self._fw_pre_hook)
         self._fw_post_handle = register_module_forward_hook(super()._fw_post_hook)
 
     def __exit__(self, *args):
         super().__exit__(*args)
+
+    def print_paramater_info(self):
+        print(self.module_parameters_dict)
+
+    def print_sharding_info(self):
+        for key, value in self.sharding_dict.items():
+            print(key + ": " + str(value))
