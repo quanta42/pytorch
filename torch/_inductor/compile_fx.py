49031103aef3ecec8f0e159f538dbfc13ec22c2e
Wang, Eikan <eikan.wang@intel.com>
2024-06-05 15:02:27 +0000

    Support aten operations with out tensor
    ghstack-source-id: a9928540accb480c13f0d862a1042763d6edc4a4
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/124926

--- torch/_inductor/compile_fx.py
+++ torch/_inductor/compile_fx.py
@@ -12,7 +12,6 @@ from typing import Any, Callable, Dict, List, Optional, Sequence, Tuple, Union
 from unittest import mock
 
 import torch._inductor.async_compile  # noqa: F401 required to warm up AsyncCompile pools
-
 import torch.fx
 import torch.utils._pytree as pytree
 
@@ -49,6 +48,9 @@ from torch._logging import trace_structured
 from torch._ops import OpOverload
 from torch._subclasses.fake_tensor import FakeTensor
 from torch._utils_internal import compile_time_strobelight_meta
+from torch.export._trace import _make_argument_spec
+
+from torch.export.graph_signature import _sig_to_specs, OutputKind
 from torch.fx.experimental.symbolic_shapes import free_unbacked_symbols
 from torch.fx.passes.fake_tensor_prop import FakeTensorProp
 
@@ -160,6 +162,7 @@ def _unlift_graph(mod, gm, graph_signature):
         )
 
     placeholder_nodes = gm.graph.find_nodes(op="placeholder")
+    placeholder_node_list = {}
     lifted_inputs = []
 
     # In AOTI, module parameters and buffers are not lifted as graph inputs.
@@ -169,6 +172,7 @@ def _unlift_graph(mod, gm, graph_signature):
     # support training.
     for node in placeholder_nodes:
         node_name = node.name
+        placeholder_node_list[node_name] = node
         if node_name in graph_signature.inputs_to_parameters:
             parameter_name = graph_signature.inputs_to_parameters[node_name]
             lifted_inputs.append(parameter_name)
@@ -184,13 +188,40 @@ def _unlift_graph(mod, gm, graph_signature):
 
     from torch.export._unlift import _unlift
 
-    outputs = list(gm.graph.nodes)[-1].args[0]
-    mutated_outputs = []
-    for out in outputs:
-        if out.name in graph_signature.buffers_to_mutate:
-            mutated_outputs.append(graph_signature.buffers_to_mutate[out.name])
-        else:
-            mutated_outputs.append(None)
+    _, output_specs = _sig_to_specs(
+        user_inputs=set(graph_signature.user_inputs),
+        inputs_to_parameters=graph_signature.inputs_to_parameters,  # type: ignore[arg-type]
+        inputs_to_buffers=graph_signature.inputs_to_buffers,  # type: ignore[arg-type]
+        user_outputs=set(graph_signature.user_outputs),  # type: ignore[arg-type]
+        buffer_mutations=graph_signature.buffers_to_mutate,  # type: ignore[arg-type]
+        user_input_mutations=graph_signature.user_inputs_to_mutate,  # type: ignore[arg-type]
+        grad_params={},  # type: ignore[arg-type, union-attr]
+        grad_user_inputs={},  # type: ignore[arg-type, union-attr]
+        loss_output=None,  # type: ignore[arg-type, union-attr]
+        inputs=[
+            _make_argument_spec(i, node, graph_signature.input_tokens)
+            for i, node in enumerate(gm.graph.nodes)
+            if node.op == "placeholder"
+        ],
+        outputs=[
+            _make_argument_spec(i, node, graph_signature.input_tokens)
+            for i, node in enumerate(
+                pytree.tree_leaves(next(iter(reversed(gm.graph.nodes))).args)
+            )
+        ],
+        input_tokens=graph_signature.input_tokens,
+        output_tokens=graph_signature.output_tokens,
+    )
+
+    mutated_outputs: List[Optional[str]] = [
+        (
+            out_spec.target
+            if out_spec.kind
+            in (OutputKind.BUFFER_MUTATION, OutputKind.USER_INPUT_MUTATION)
+            else None
+        )
+        for out_spec in output_specs
+    ]
 
     unlifted_gm = _unlift(
         gm,
@@ -201,6 +232,7 @@ def _unlift_graph(mod, gm, graph_signature):
         state_dict,
         {},
     )
+
     return unlifted_gm
 
 
